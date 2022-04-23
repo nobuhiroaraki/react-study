862fcbe4876728641e382e1665ed5b2d788aac08
nobu_mac <nobuhiro.araki0513@gmail.com>
2022-04-23 17:02:58 +0900

    setting routing each pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import React from "react";
-import { ChakraProvider, Button } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
+
 import { theme } from "./theme/theme";
+import { Router } from "./router/Router";
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Button colorScheme="teal">テストボタン</Button>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </ChakraProvider>
   );
 }
