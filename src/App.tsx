d91d987b8d88f31a9f15b506245b71f4e28ce994
nobu_mac <nobuhiro.araki0513@gmail.com>
2022-04-23 14:08:41 +0900

    add pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import { ChakraProvider,Button } from '@chakra-ui/react';
-import {theme} from "./theme/theme"
+import React from "react";
+import { ChakraProvider, Button } from "@chakra-ui/react";
+import { theme } from "./theme/theme";
 
 function App() {
   return (
-    <ChakraProvider theme = {theme}>
+    <ChakraProvider theme={theme}>
       <Button colorScheme="teal">テストボタン</Button>
     </ChakraProvider>
   );
