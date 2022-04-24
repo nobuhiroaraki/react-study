d61305682310949f18e7a636ac78c25f1284badf
nobu_mac <nobuhiro.araki0513@gmail.com>
2022-04-24 21:09:38 +0900

    unsplashとjsonplaceholderから取得した情報をユーザー管理ページに表示する

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,19 +1,34 @@
-import { FC, memo } from "react";
-import { Wrap, WrapItem } from "@chakra-ui/react";
+import { FC, memo, useEffect } from "react";
+import { Center, Spinner, Wrap, WrapItem } from "@chakra-ui/react";
 import { UserCard } from "../organisms/layout/user/UserCard";
+import { useAllUsers } from "../../hooks/useAllUsers";
+import { Header } from "../organisms/layout/Header";
 
 export const UserManagement: FC = memo(() => {
+  const { getUsers, users, loading } = useAllUsers();
+  //初回実行時のみgetUsersする
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(() => getUsers(), []);
   return (
-    <Wrap spacing="30px" p={{ base: 4, md: 10 }}>
-      {[...Array(100)].map(() => (
-        <WrapItem>
-          <UserCard
-            imageUrl="https://source.unsplash.com/random"
-            name="ニックネーム"
-            fullName="フルネーム"
-          />
-        </WrapItem>
-      ))}
-    </Wrap>
+    <>
+      <Header />
+      {loading ? (
+        <Center h="100vh">
+          <Spinner />
+        </Center>
+      ) : (
+        <Wrap p={{ base: 4, md: 10 }}>
+          {users.map((user) => (
+            <WrapItem key={user.id} mx="center">
+              <UserCard
+                imageUrl={`https://source.unsplash.com/random/${user.id}`}
+                name={user.username}
+                fullName={user.name}
+              />
+            </WrapItem>
+          ))}
+        </Wrap>
+      )}
+    </>
   );
 });
