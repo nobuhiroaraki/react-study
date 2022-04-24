388ce91812f9d01a403fbda97a89cf17060fb8e8
nobu_mac <nobuhiro.araki0513@gmail.com>
2022-04-24 16:32:17 +0900

    chakra-uiでalertを表示するようにした

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,25 +1,38 @@
+import { useMessage } from "./useMessage";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useCallback, useState } from "react";
 import { User } from "../types/api/user";
 export const useAuth = () => {
   const navigate = useNavigate();
+  const { showMessage } = useMessage();
   const [loading, setLoading] = useState<boolean>(false);
-  const login = useCallback((id: string) => {
-    setLoading(true);
-    axios
-      .get<User>(`https://jsonplaceholder.typicode.com/users/${id}`)
-      .then((res) => {
-        if (res.data) {
-          navigate("/home");
-          return;
-        } else {
-          alert("データを取得できませんでした");
-        }
-      })
-      .catch(() => alert("ユーザーが見つかりませんでした"))
-      .finally(() => setLoading(false));
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  const login = useCallback(
+    (id: string) => {
+      setLoading(true);
+      axios
+        .get<User>(`https://jsonplaceholder.typicode.com/users/${id}`)
+        .then((res) => {
+          if (res.data) {
+            showMessage({ title: "ログインしました", status: "success" });
+            navigate("/home");
+            return;
+          } else {
+            showMessage({
+              title: "データを取得できませんでした",
+              status: "error",
+            });
+          }
+        })
+        .catch(() =>
+          showMessage({
+            title: "ユーザーIDが見つかりませんでした",
+            status: "warning",
+          })
+        )
+        .finally(() => setLoading(false));
+    },
+    [navigate, showMessage]
+  );
   return { login, loading };
 };
